ce7c0cbd1abe9ea5a0e1a33986ec50378aab48a7
meta-siddhi-agarwal <siddhi.agarwal@metacube.com>
2025-03-10 12:28:25 +0530

    Fcfs.java

--- Fcfs.java
+++ Fcfs.java
@@ -2,24 +2,90 @@ package pac;
 import java.util.*;
 
 public class Fcfs{
-int p[][];
+static int p[][];
 
+//arraylist to hold completion time for a process
+static List<Integer> comp=new ArrayList<>();
+
+//arraylist to hold waiting time for a process
+static List<Integer> wait=new ArrayList<>();
+
+static Scanner sc=new Scanner(System.in);
 
 public static void main(String args[]){
-    Scanner sc=new Scanner(System.in);
-    Fcfs f=new Fcfs();
-System.out.print("Enter  no. of processes");
+	try 
+	{
+System.out.print("Enter no. of processes ");
 int size=sc.nextInt();
-f.p=new int[size][2];
+p=new int[size][2];
+
+//loop will take inputs in a 2-D array
 for(int i=0;i<size;i++) {
 int process=i+1;
-    System.out.println("Enter arrivial time for process "+process);
-f.p[i][0]=sc.nextInt();
+System.out.println("Enter arrivial time for process "+process);
+int arrivial_time=sc.nextInt();
+if(arrivial_time>=0)p[i][0]=arrivial_time;
+else {
+	System.out.println("Please enter valid input");
+    return;
+}
 System.out.println("Enter burst time for process "+(i+1));
-f.p[i][1]=sc.nextInt();
+int burst_time=sc.nextInt();
+if(burst_time>=0)p[i][1]=burst_time;
+else {
+	System.out.println("Please enter valid input");
+	return;
 }
+comp.add(0);
+wait.add(0);
 }
-    List<Integer> complete(int[][] arr){
+
+//Giving options to user
+System.out.println("Choose which operation to perform\n"
++"1. Calculate completion time for each process\n"
++"2. Calculate waiting time for each process\n"
++"3. Calculate turn around time for each process\n"
++"4. Calculate avg. waiting time \n"
++"5. Max. waiting time for a process");
+int input=sc.nextInt();
+
+//user will enter input
+switch(input) {
+case 1:
+	comp=complete(p);
+	System.out.println(comp);
+	break;
+case 2:
+	comp=complete(p);
+	List<Integer> wait=wait(p,comp);
+	System.out.println(wait);
+	break;
+case 3:
+	comp=complete(p);
+	List<Integer> turn=turn(p,comp);
+	System.out.println(turn);
+	break;
+case 4:
+	wait=wait(p,comp);
+	System.out.println(avg_wait(wait));
+	break;
+case 5:
+	wait=wait(p,comp);
+	System.out.println(max_wait(wait));
+	break;
+default:
+	System.out.println("Please enter valid input");
+}
+	}
+catch(Exception e) 
+	{
+	System.out.println("Please enter valid input");
+}
+
+}
+
+//cal. completion for each process
+    static List<Integer> complete(int[][] arr){
         List<Integer> ls=new ArrayList<>();
         int initialc=arr[0][0];
         for(int i=0;i<arr.length;i++){
@@ -28,7 +94,9 @@ f.p[i][1]=sc.nextInt();
         }
         return ls;
     }
-List<Integer>   wait(int arr[][],List<Integer> ls){
+    
+//cal. waiting for each process
+static List<Integer> wait(int arr[][],List<Integer> ls){
     List<Integer> ls1=new ArrayList<>();
         for(int i=0;i<arr.length;i++){
             int turn=ls.get(i)-arr[i][1];
@@ -37,14 +105,18 @@ List<Integer>   wait(int arr[][],List<Integer> ls){
         }
         return ls1;
     }
-List<Integer> turn(int arr[][],List<Integer> ls){
+
+//cal. turn around time for each process
+static List<Integer> turn(int arr[][],List<Integer> ls){
     List<Integer> ls2=new ArrayList<>();
     for(int i=0;i<arr.length;i++){
         ls2.add(ls.get(i)-arr[i][0]);
     }
     return ls2;
 }
-double avg(List<Integer> l){
+
+//cal. avg. waiting time
+static double avg_wait(List<Integer> l){
     double average;
     int total=0;
     for(int i=0;i<l.size();i++){
@@ -54,7 +126,9 @@ double avg(List<Integer> l){
     average=total/l.size();
     return average;
 }
-int max_wait(List<Integer> ls){
+
+//cal. max. waiting time
+static int max_wait(List<Integer> ls){
     int max=Integer.MIN_VALUE;
     for(int i=0;i<ls.size();i++){
         max=Math.max(max,ls.get(i));
